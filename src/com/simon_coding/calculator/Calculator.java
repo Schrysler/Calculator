4df2e4fd8dc3328083345e6b60a4247a5452c11b
Schrysler <Schrysler@Schrysler-Sub>
2011-04-12 14:11:47 -0300

    Base point

---
+++ src/com/simon_coding/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.simon_coding.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Calculator extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
